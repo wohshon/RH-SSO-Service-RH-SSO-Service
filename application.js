217abcb46eb5a4a8893373f2d9c58ff29662706a
Woh Shon <wohshon@gmail.com>
2017-02-14 18:45:39 +0800

    initial commit

--- application.js
+++ application.js
@@ -27,7 +27,7 @@ app.use('/hello', require('./lib/hello.js')());
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8012;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
